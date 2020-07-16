42955bbfc8638c7f23ec3531df9ba299bc895bed
Piranja <techpiranja@gmail.com>
2020-07-17 00:24:32 +0200

    ! fixed orientation of bar chart

--- scripts/script.js
+++ scripts/script.js
@@ -8,22 +8,24 @@ const container = d3.select("svg").classed("container", true);
 
 const render = (data) => {
 	const xScale = d3
-		.scaleLinear()
-		.domain([0, d3.max(data, (d) => d.new_cases)])
-		.range([0, 250]);
-	const yScale = d3
 		.scaleBand()
 		.domain(data.map((d) => d.date))
-		.range([0, 250]);
-	const bars = container
+		.range([0, 250])
+		.padding(0.1);
+	const yScale = d3
+		.scaleLinear()
+		.domain([0, d3.max(data, (d) => d.new_cases)])
+		.range([250, 0]);
+	container
 		.selectAll(".bar")
 		.data(data)
 		.enter()
 		.append("rect")
 		.classed("bar", true)
-		.attr("width", (d) => xScale(d.new_cases))
-		.attr("height", (data = yScale.bandwidth()))
-		.attr("y", (data) => yScale(data.date));
+		.attr("width", xScale.bandwidth())
+		.attr("height", (d) => 250 - yScale(d.new_cases))
+		.attr("x", (d) => xScale(d.date))
+		.attr("y", (d) => yScale(d.new_cases));
 };
 
 d3.csv("../test.csv").then((data) => {
