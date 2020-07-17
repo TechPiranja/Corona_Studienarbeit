7fafcde62f843c52558012538489d59fd8cfcf05
Piranja <techpiranja@gmail.com>
2020-07-17 13:58:37 +0200

    / managed to add tickValues to x-Axis and resized axis labels

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,3 @@
-const Dummy_Data = [
-	{ id: "d1", value: 10, region: "USA" },
-	{ id: "d2", value: 5, region: "Germany" },
-	{ id: "d3", value: 7, region: "China" },
-];
-
 const container = d3.select("svg").classed("container", true);
 
 const render = (data) => {
@@ -11,17 +5,22 @@ const render = (data) => {
 	const height = parseInt(container.style("height"));
 	const xValue = (d) => d.date;
 	const yValue = (d) => d.new_cases;
-	const margin = { top: 20, right: 20, bottom: 20, left: 20 };
+	const margin = { top: 20, right: 20, bottom: 20, left: 30 };
 	const innerWidth = width - margin.left - margin.right;
 	const innerHeight = height - margin.top - margin.bottom;
 
-	const xScale = d3.scaleBand().domain(data.map(xValue)).range([0, innerWidth]).padding(0.1);
+	const xScale = d3.scaleBand().domain(data.map(xValue)).range([0, innerWidth]).padding(0.2);
 	const yScale = d3
 		.scaleLinear()
 		.domain([0, d3.max(data, yValue)])
 		.range([innerHeight, 0]);
-
 	const g = container.append("g").attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+	g.append("g").style("font", "5px times").call(d3.axisLeft(yScale));
+	g.append("g")
+		.style("font", "5px times")
+		.attr("transform", `translate(0, ${innerHeight})`)
+		.call(d3.axisBottom(xScale).tickValues(xScale.domain().filter((d, i) => i % 30 === 0)));
 
 	g.selectAll(".bar")
 		.data(data)
