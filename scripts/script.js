12cfa1884397edbd0d8681f29c18127def56868d
Piranja <techpiranja@gmail.com>
2020-07-18 19:16:52 +0200

    / bar now shows only DEU, and line chart all groups

--- scripts/script.js
+++ scripts/script.js
@@ -3,14 +3,14 @@ const container = d3.select("#simpleBarChart").classed("container", true);
 const render = (data) => {
 	const width = parseInt(container.style("width"));
 	const height = parseInt(container.style("height"));
-	const xValue = (d) => d.date;
-	const yValue = (d) => d.DEU_new_cases;
+	var xValue = (d) => d.date;
+	var yValue = (d) => d.Country == "DEU" && d.new_cases;
 	const margin = { top: 20, right: 50, bottom: 40, left: 50 };
 	const innerWidth = width - margin.left - margin.right;
 	const innerHeight = height - margin.top - margin.bottom;
 
 	const xScale = d3.scaleBand().domain(data.map(xValue)).range([0, innerWidth]).padding(0.1);
-	const yScale = d3
+	var yScale = d3
 		.scaleLinear()
 		.domain([0, d3.max(data, yValue)])
 		.range([innerHeight, 0]);
@@ -39,6 +39,11 @@ const render = (data) => {
 		.attr("y", (d) => yScale(yValue(d)));
 
 	//------------ second visualization ------------
+	yValue = (d) => d.new_cases;
+	yScale = d3
+		.scaleLinear()
+		.domain([0, d3.max(data, yValue)])
+		.range([innerHeight, 0]);
 	const dataviz = d3
 		.select("#dataviz")
 		.append("svg")
@@ -50,10 +55,9 @@ const render = (data) => {
 	var sumstat = d3
 		.nest() // nest function allows to group the calculation per level of a factor
 		.key(function (d) {
-			return d.key;
+			return d.Country;
 		})
 		.entries(data);
-
 	dataviz
 		.append("g")
 		.style("font", "15px times")
@@ -87,7 +91,7 @@ const render = (data) => {
 		.attr("stroke", function (d) {
 			return color(d.key);
 		})
-		.attr("stroke-width", 5)
+		.attr("stroke-width", 1)
 		.attr("d", function (d) {
 			return d3
 				.line()
@@ -95,15 +99,14 @@ const render = (data) => {
 					return xScale(d.date);
 				})
 				.y(function (d) {
-					return yScale(+d.new_cases);
+					return yScale(d.new_cases);
 				})(d.values);
 		});
 };
 
 d3.csv("../notebooks/test.csv").then((data) => {
 	data.forEach((d) => {
-		d.DEU_new_cases = +d.DEU_new_cases;
-		d.USA_new_cases = +d.USA_new_cases;
+		d.new_cases = +d.new_cases;
 	});
 	console.log(data);
 	render(data);
